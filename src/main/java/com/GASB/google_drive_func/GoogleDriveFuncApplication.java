ba0043ddcd6370df42faca91f24ea2d5eb9a1fbf
2W <hsp003636@gmail.com>
2024-08-08 19:25:20 +0900

    init

---
+++ src/main/java/com/GASB/google_drive_func/GoogleDriveFuncApplication.java
@@ -0,0 +1,13 @@
+package com.GASB.google_drive_func;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDriveFuncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleDriveFuncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GASB/google_drive_func/GoogleDriveFuncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GASB.google_drive_func;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleDriveFuncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
