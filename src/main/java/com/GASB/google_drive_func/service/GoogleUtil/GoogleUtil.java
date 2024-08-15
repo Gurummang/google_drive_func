37746dce6893fc722f0bd33b3ce8911fc916177d
2W <hsp003636@gmail.com>
2024-08-16 08:35:04 +0900

    main <fix : Google Config fix>

--- src/main/java/com/GASB/google_drive_func/service/GoogleUtil/GoogleUtil.java
+++ src/main/java/com/GASB/google_drive_func/service/GoogleUtil/GoogleUtil.java
@@ -27,12 +27,11 @@ public class GoogleUtil {
     private static final String APPLICATION_NAME = "grummang-google-dirve-func";
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
 
-    private final GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow;
+//    private final GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow;
     private final WorkspaceConfigRepo workspaceConfigRepo;
 
     @Autowired
-    public GoogleUtil(GoogleAuthorizationCodeFlow googleAuthorizationCodeFlow, WorkspaceConfigRepo workspaceConfigRepo) {
-        this.googleAuthorizationCodeFlow = googleAuthorizationCodeFlow;
+    public GoogleUtil(WorkspaceConfigRepo workspaceConfigRepo) {
         this.workspaceConfigRepo = workspaceConfigRepo;
     }
 
@@ -51,31 +50,31 @@ public class GoogleUtil {
         }
     }
 
-    private TokenResponse refreshToken(GoogleAuthorizationCodeFlow flow, String refreshToken) throws IOException {
-        return flow.newTokenRequest(refreshToken).setGrantType("refresh_token").execute();
-    }
-
-    private Credential refreshAccessToken(Credential credential, int workspace_id) throws IOException {
-        TokenResponse response = refreshToken(googleAuthorizationCodeFlow, credential.getRefreshToken());
-        log.info("Access token refreshed successfully.");
-        credential.setAccessToken(response.getAccessToken());
-        credential.setExpiresInSeconds(response.getExpiresInSeconds());
-        // 수정: 새로운 토큰을 DB에 저장
-        workspaceConfigRepo.updateToken(workspace_id, response.getAccessToken());
-        return credential;
-    }
-
-    protected Credential getCredentials() throws Exception {
-
-        // 기존의 자격 증명을 무시하고 항상 새로 발급받도록 함
-        NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8088).setCallbackPath("/login/oauth2/code/google").build();
-
-        // 새로 발급된 자격 증명을 DB에 저장
-//        workspaceConfigRepo.updateToken(workspace_id, credential.getAccessToken());
-
-        return new AuthorizationCodeInstalledApp(googleAuthorizationCodeFlow, receiver).authorize("user");
-    }
+//    private TokenResponse refreshToken(GoogleAuthorizationCodeFlow flow, String refreshToken) throws IOException {
+//        return flow.newTokenRequest(refreshToken).setGrantType("refresh_token").execute();
+//    }
+//
+//    private Credential refreshAccessToken(Credential credential, int workspace_id) throws IOException {
+//        TokenResponse response = refreshToken(googleAuthorizationCodeFlow, credential.getRefreshToken());
+//        log.info("Access token refreshed successfully.");
+//        credential.setAccessToken(response.getAccessToken());
+//        credential.setExpiresInSeconds(response.getExpiresInSeconds());
+//        // 수정: 새로운 토큰을 DB에 저장
+//        workspaceConfigRepo.updateToken(workspace_id, response.getAccessToken());
+//        return credential;
+//    }
+//
+//    protected Credential getCredentials() throws Exception {
+//
+//        // 기존의 자격 증명을 무시하고 항상 새로 발급받도록 함
+//        NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+//        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8088).setCallbackPath("/login/oauth2/code/google").build();
+//
+//        // 새로 발급된 자격 증명을 DB에 저장
+////        workspaceConfigRepo.updateToken(workspace_id, credential.getAccessToken());
+//
+//        return new AuthorizationCodeInstalledApp(googleAuthorizationCodeFlow, receiver).authorize("user");
+//    }
 
 
     public Drive getDriveService(int workspace_id) throws Exception {
