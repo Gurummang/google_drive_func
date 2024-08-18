a6beb03a3a3142063ff04cd8218ad2375aa46386
2W <hsp003636@gmail.com>
2024-08-18 23:50:48 +0900

    DEV <fix : 명시적인 어노테이션 설정>

--- src/main/java/com/GASB/google_drive_func/model/repository/org/WorkspaceConfigRepo.java
+++ src/main/java/com/GASB/google_drive_func/model/repository/org/WorkspaceConfigRepo.java
@@ -1,7 +1,9 @@
 package com.GASB.google_drive_func.model.repository.org;
 
 import com.GASB.google_drive_func.model.entity.WorkspaceConfig;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -14,6 +16,8 @@ public interface WorkspaceConfigRepo extends JpaRepository<WorkspaceConfig, Stri
     Optional<WorkspaceConfig> findById(@Param("id") int id);
 
     // Update Token
+    @Modifying
+    @Transactional
     @Query("update WorkspaceConfig w set w.token = :token where w.id = :id")
     void updateToken(@Param("id") int id,@Param("token") String token);
 }
