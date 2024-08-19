3db6352b8e075203cb890a48c3ab209121799282
2W <hsp003636@gmail.com>
2024-08-19 18:06:05 +0900

    DEV <fix : 안쓰는 쿼리문 제거>

--- src/main/java/com/GASB/google_drive_func/model/repository/files/FileUploadRepository.java
+++ src/main/java/com/GASB/google_drive_func/model/repository/files/FileUploadRepository.java
@@ -18,24 +18,10 @@ public interface FileUploadRepository extends JpaRepository<FileUploadTable, Lon
             "JOIN OrgSaaS os ON fu.orgSaaS.id = os.id " +
             "JOIN Activities a ON fu.saasFileId = a.saasFileId " +
             "JOIN StoredFile sf ON fu.hash = sf.saltedHash " +
-            "JOIN MonitoredUsers u ON a.user.userId = u.userId " +
+            "JOIN MonitoredUsers u ON a.user.id = u.id " +
             "WHERE os.org.id = :orgId AND os.saas.id = :saasId " +
             "ORDER BY fu.timestamp DESC LIMIT 10")
     List<DriveRecentFileDTO> findRecentFilesByOrgIdAndSaasId(@Param("orgId") int orgId, @Param("saasId") int saasId);
-
-    // Corrected method to find by OrgSaaS fields
-
-    Optional<FileUploadTable> findBySaasFileIdAndTimestamp(String saasFileId, LocalDateTime timestamp);
-
-    @Query("UPDATE FileUploadTable fu " +
-            "SET fu.deleted = true " +
-            "WHERE fu.saasFileId = :saasFileId")
-    void checkDelete(@Param("saasFileId") String saasFileId);
-
-
-    @Query("SELECT CASE WHEN COUNT(fu) > 0 THEN TRUE ELSE FALSE END FROM FileUploadTable fu WHERE fu.saasFileId = :id AND fu.timestamp = :timestamp")
-    boolean existsObjectById(@Param("id") String SaasFileId, @Param("timestamp") LocalDateTime timestamp);
-
 
     boolean existsBySaasFileIdAndTimestamp(String saasFileId, LocalDateTime timestamp);
 }
