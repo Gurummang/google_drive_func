4ce0e90152f622e8f9a84677bb3892599a0a13e6
2W <hsp003636@gmail.com>
2024-08-19 17:45:28 +0900

    DEV <fix : 중복처리 추가>

--- src/main/java/com/GASB/google_drive_func/model/repository/files/StoredFileRepository.java
+++ src/main/java/com/GASB/google_drive_func/model/repository/files/StoredFileRepository.java
@@ -43,14 +43,14 @@ public interface StoredFileRepository extends JpaRepository<StoredFile, Long> {
 
 
 
-    @Query("SELECT COUNT(fu.id) FROM Org o "+
+    @Query("SELECT COUNT(DISTINCT fu.id) FROM Org o "+
             "INNER JOIN OrgSaaS os ON o.id = os.org.id "+
             "INNER JOIN Saas s ON os.saas.id = s.id "+
             "INNER JOIN FileUploadTable fu ON os.id = fu.orgSaaS.id "+
             "WHERE o.id = :orgId AND s.id = :saasId AND fu.deleted != true")
     int countTotalFiles(@Param("orgId") int orgId, @Param("saasId") int saasId);
 
-    @Query("SELECT COUNT(fu.id) FROM Org o " +
+    @Query("SELECT COUNT(DISTINCT fu.id) FROM Org o " +
             "INNER JOIN OrgSaaS os ON o.id = os.org.id " +
             "INNER JOIN Saas s ON os.saas.id = s.id " +
             "INNER JOIN FileUploadTable fu ON os.id = fu.orgSaaS.id " +
@@ -58,7 +58,7 @@ public interface StoredFileRepository extends JpaRepository<StoredFile, Long> {
             "WHERE o.id = :orgId AND s.id = :saasId AND dr.dlp = true AND fu.deleted != true")
     int countSensitiveFiles(@Param("orgId") int orgId, @Param("saasId") int saasId);
 
-    @Query("SELECT COUNT(fu.id) FROM Org o " +
+    @Query("SELECT COUNT(DISTINCT fu.id) FROM Org o " +
             "INNER JOIN OrgSaaS os ON o.id = os.org.id " +
             "INNER JOIN Saas s ON os.saas.id = s.id " +
             "INNER JOIN FileUploadTable fu ON os.id = fu.orgSaaS.id " +
@@ -66,7 +66,7 @@ public interface StoredFileRepository extends JpaRepository<StoredFile, Long> {
             "WHERE o.id = :orgId AND s.id = :saasId AND vr.threatLabel != 'none' AND fu.deleted != true")
     int countMaliciousFiles(@Param("orgId") int orgId, @Param("saasId") int saasId);
 
-    @Query("SELECT COUNT(mu.userId) FROM Org o " +
+    @Query("SELECT COUNT(DISTINCT mu.userId) FROM Org o " +
             "INNER JOIN OrgSaaS os ON o.id = os.org.id " +
             "INNER JOIN Saas s ON os.saas.id = s.id " +
             "INNER JOIN MonitoredUsers mu ON os.id = mu.orgSaaS.id " +
