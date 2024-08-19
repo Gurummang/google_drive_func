8314f83843fc7fc7df323ed6056da669a303869b
2W <hsp003636@gmail.com>
2024-08-19 19:40:47 +0900

    DEV <fix : 쿼리 업데이트>

--- src/main/java/com/GASB/google_drive_func/model/repository/files/FileUploadRepository.java
+++ src/main/java/com/GASB/google_drive_func/model/repository/files/FileUploadRepository.java
@@ -23,5 +23,7 @@ public interface FileUploadRepository extends JpaRepository<FileUploadTable, Lon
             "ORDER BY fu.timestamp DESC LIMIT 10")
     List<DriveRecentFileDTO> findRecentFilesByOrgIdAndSaasId(@Param("orgId") int orgId, @Param("saasId") int saasId);
 
-    boolean existsBySaasFileIdAndTimestamp(String saasFileId, LocalDateTime timestamp);
+    @Query("SELECT CASE WHEN COUNT(f) > 0 THEN true ELSE false END FROM FileUploadTable f WHERE f.saasFileId = :saasFileId AND f.timestamp = :timestamp")
+    boolean existsBySaasFileIdAndTimestamp(@Param("saasFileId") String saasFileId, @Param("timestamp") LocalDateTime timestamp);
+
 }
