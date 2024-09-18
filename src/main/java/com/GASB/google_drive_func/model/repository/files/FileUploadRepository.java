bda0196ae7718888a6f8798604c34c7548a5ced0
2W <hsp003636@gmail.com>
2024-09-19 00:04:13 +0900

    KAN-478 <add : 삭제 컬럼 변경하는 쿼리문 추가>

--- src/main/java/com/GASB/google_drive_func/model/repository/files/FileUploadRepository.java
+++ src/main/java/com/GASB/google_drive_func/model/repository/files/FileUploadRepository.java
@@ -2,7 +2,9 @@ package com.GASB.google_drive_func.model.repository.files;
 
 import com.GASB.google_drive_func.model.dto.file.DriveRecentFileDTO;
 import com.GASB.google_drive_func.model.entity.FileUploadTable;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -29,5 +31,15 @@ public interface FileUploadRepository extends JpaRepository<FileUploadTable, Lon
     @Query("SELECT fu FROM FileUploadTable fu WHERE fu.hash = :file_hash AND fu.id = :idx")
     Optional<FileUploadTable> findByIdAndFileHash(@Param("idx") int idx, @Param("file_hash")String file_hash);
 
+
+    @Query("SELECT fu.hash FROM FileUploadTable fu WHERE fu.saasFileId = :saas_file_id")
+    Optional<String> findFileHashByFileId(@Param("saas_file_id")String file_id);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE FileUploadTable fu " +
+            "SET fu.deleted = true " +
+            "WHERE fu.saasFileId = :saasFileId AND fu.id IS NOT NULL")
+    void checkDelete(@Param("saasFileId") String saasFileId);
 
 }
