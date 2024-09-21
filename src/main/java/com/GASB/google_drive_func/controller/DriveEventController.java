44e4f0e91692f6aaffdf00ae856d78fd82abff1c
2W <hsp003636@gmail.com>
2024-09-21 17:10:36 +0900

    KAN-501 <fix : 이벤트 파싱 수정>

--- src/main/java/com/GASB/google_drive_func/controller/DriveEventController.java
+++ src/main/java/com/GASB/google_drive_func/controller/DriveEventController.java
@@ -1,6 +1,9 @@
 package com.GASB.google_drive_func.controller;
 
+import com.GASB.google_drive_func.service.DriveApiService;
+import com.GASB.google_drive_func.service.GoogleUtil.GoogleUtil;
 import com.GASB.google_drive_func.service.event.GoogleDriveEvent;
+import com.google.api.services.drive.Drive;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,25 +17,33 @@ import java.util.Map;
 public class DriveEventController {
 
     private final GoogleDriveEvent googleDriveEvent;
+    private final GoogleUtil googleUtil;
+    private final DriveApiService driveApiService;
 
     @Autowired
-    public DriveEventController(GoogleDriveEvent googleDriveEvent) {
+    public DriveEventController(GoogleDriveEvent googleDriveEvent, GoogleUtil googleUtil, DriveApiService driveApiService) {
         this.googleDriveEvent = googleDriveEvent;
+        this.googleUtil = googleUtil;
+        this.driveApiService = driveApiService;
     }
     @PostMapping("/file-change")
     public ResponseEntity<String> handleFileChangedEvent(@RequestBody Map<String, Object> payload) throws Exception {
         log.info("File changed event received: {}", payload);
-        String eventType = payload.get("resourceState").toString();
+
+        int workspaceId = Integer.parseInt(payload.get("workspaceId").toString());
+        String file_id = payload.get("resourceId").toString();
+        Drive service = googleUtil.getDriveService(workspaceId);
+        String eventType = driveApiService.getFileDetails(file_id, service);
         switch (eventType) {
-            case "add" -> {
+            case "create" -> {
                 log.info("File create event received");
                 googleDriveEvent.handledCreateEvent(payload);
             }
-            case "update" -> {
+            case "modify" -> {
                 log.info("File update event received");
                 googleDriveEvent.handledUpdateEvent(payload);
             }
-            case "remove" -> {
+            case "delete" -> {
                 log.info("File delete event received");
                 googleDriveEvent.handledDeleteEvent(payload);
             }
