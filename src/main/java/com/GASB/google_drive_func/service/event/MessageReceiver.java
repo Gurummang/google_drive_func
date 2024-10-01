ae3cb40a11d8b444da3b60cf16cbc40448b67609
2W <hsp003636@gmail.com>
2024-10-01 15:39:37 +0900

    DEV <fix : 삭제 메세지를 받는 리시버 추가 >

--- src/main/java/com/GASB/google_drive_func/service/event/MessageReceiver.java
+++ src/main/java/com/GASB/google_drive_func/service/event/MessageReceiver.java
@@ -1,30 +1,45 @@
 package com.GASB.google_drive_func.service.event;
 
+import com.GASB.google_drive_func.service.file.DriveFileService;
 import com.GASB.google_drive_func.service.init.DriveInitService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @Component
 @Slf4j
 public class MessageReceiver {
 
     private final DriveInitService driveInitService;
+    private final DriveFileService driveFileService;
 
     @Autowired
-    public MessageReceiver(DriveInitService driveInitService) {
+    public MessageReceiver(DriveInitService driveInitService, DriveFileService driveFileService) {
         this.driveInitService = driveInitService;
+        this.driveFileService = driveFileService;
     }
     @RabbitListener(queues = "${rabbitmq.init.queue}")
     public void receiveMessage(int message) {
         try {
             log.info("Received message from queue: " + message);
             driveInitService.fetchAndSaveAll(message);
-        } catch (IllegalArgumentException e) {
+        } catch (RuntimeException e) {
             log.error("Error receiving message: {}", e.getMessage());
+        }
+    }
+
+
+    @RabbitListener(queues = "${rabbitmq.O365_DELETE_QUEUE}")
+    public void receiveDeleteMessage(List<Map<String,String>> message){
+        try {
+            driveFileService.fileDelete(message);
         } catch (Exception e) {
             log.error("Error receiving message: {}", e.getMessage());
         }
     }
+
 }
